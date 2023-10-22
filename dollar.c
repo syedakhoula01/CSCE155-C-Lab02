9a72eb56b3d985a0e20701f43114073fead8113d
syeda khoula abid <syeda.khoula.abid234@gmail.com>
2023-10-22 21:59:50 +0500

    dollar: asking for amount in US dollar

---
+++ dollar.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    double USD_amount , GBP_amount , JPY_amount , exchange_fee;
+
+    printf("enter the amount in US dollar: ");
+    scanf("%lf", &USD_amount);
+}
